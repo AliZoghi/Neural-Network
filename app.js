181e653642ead6e7cfd884ca0781e7e11e962932
unknown <C:\Users\Ali\AppData\Roaming\The Bat!>
2022-06-09 05:50:30 +0430

    connect funtionality to ui

--- app.js
+++ app.js
@@ -1,118 +1,217 @@
 "use strcit";
 
-let currentBios = 0;
-const learningCurve = 1;
-
-const changeWeight = (w, x, t) => {
-  //   console.log(`w: ${w}, x: ${x}, t: ${t}`);
-  //   console.log(w + learningCurve * x * t);
-  return w + learningCurve * x * t;
+// Constant
+const GATE = {
+  AND: [
+    [1, 1, 1],
+    [-1, 1, 0],
+    [-1, 0, 1],
+    [-1, 0, 0],
+  ],
+  OR: [
+    [1, 1, 1],
+    [1, 1, 0],
+    [1, 0, 1],
+    [-1, 0, 0],
+  ],
+  NAND: [
+    [-1, 1, 1],
+    [1, 1, 0],
+    [1, 0, 1],
+    [1, 0, 0],
+  ],
+  NOR: [
+    [1, 1, 1],
+    [-1, 1, 0],
+    [-1, 0, 1],
+    [1, 0, 0],
+  ],
+  XOR: [
+    [-1, 1, 1],
+    [1, 1, 0],
+    [1, 0, 1],
+    [-1, 0, 0],
+  ],
 };
 
-const changeBios = (b, t) => {
-  return b + learningCurve * t;
+// DOM Element
+const formEl = document.querySelector(".form");
+const weight1El = document.getElementById("weight-1");
+const weight2El = document.getElementById("weight-2");
+const biosEl = document.getElementById("bios");
+const learningRateEl = document.getElementById("learning-rate");
+const counterEl = document.getElementById("counter");
+
+let LEARNING_RATE;
+let COUNTER_LIMIT;
+let currentBios;
+let currentWeights;
+const STORED_DATA_DEFAULT = {
+  xIns: [],
+  nets: [],
+  outs: [],
+  targets: [],
+  weights: [],
+  bioses: [],
+};
+// Event listeners
+formEl.addEventListener("submit", e => {
+  e.preventDefault();
+  LEARNING_RATE = +learningRateEl.value;
+  COUNTER_LIMIT = +counterEl.value;
+  currentBios = +biosEl.value;
+  currentWeights = [+weight1El.value, +weight2El.value];
+  const cardContainer = document.querySelector(".card__container");
+  cardContainer.innerHTML = "";
+  const gateEl = document.querySelector(".radio-button input:checked");
+
+  startProcess(gateEl.value);
+
+  resetInputs(weight1El, weight2El, biosEl, learningRateEl, counterEl);
+});
+
+// **Tools**
+const resetInputs = (...elements) => {
+  //   elements.forEach(el => (el.value = ""));
+  elements[0].focus();
 };
 
-const calcNet = (xIn, wIn, b) => {
+// Funtionarlity
+const changeWeight = (weight, x, target) => {
+  return weight + LEARNING_RATE * x * target;
+};
+
+const changeBios = (bios, target) => {
+  return bios + LEARNING_RATE * target;
+};
+
+const calcNet = (xIn, weights, bios) => {
   const result = xIn.reduce((acc, currentX, i) => {
-    return acc + currentX * wIn[i];
+    return acc + currentX * weights[i];
   }, 0);
-  //   console.log(result);
-  return b + result;
+  return bios + result;
 };
 const calcOut = net => {
-  //   if (net > 0.2) return 1;
-  //   if (net < -0.2) return -1;
-  //   return 0;
+  // pelekani
   if (net > 0) return 1;
   return -1;
 };
 
-const printOneRound = (xIn, alpha, net, out, weightChanged, biosChanged) => {
-  console.log(net);
-  console.log(
-    `[${xIn.join(",")},${alpha}] | ${net} | ${out} | [${weightChanged.join(
-      ","
-    )} ,${biosChanged}]`
-  );
-};
-
-const isEqualArrays = (main, lastWeight) => {
-  const result = main.every((_, i) => {
-    return main[i] === lastWeight[i];
-  });
-  return result;
-};
-
-const andQuest = [
-  [1, 1, 1],
-  [-1, 1, 0],
-  [-1, 0, 1],
-  [-1, 0, 0],
-];
-const orQuest = [
-  [1, 1, 1],
-  [1, 1, 0],
-  [1, 0, 1],
-  [-1, 0, 0],
-];
-const xOrQuest = [
-  [-1, 1, 1],
-  [1, 1, 0],
-  [1, 0, 1],
-  [-1, 0, 0],
-];
-let currentWIn = [0, 0];
-const calcOneRound = (b, [xAnswer, ...xValue], wIn) => {
+const calcOneRound = (bios, target, xValue, weights) => {
   let changedWeight = [];
   let changedBios;
-  //   console.log(wIn);
-  const net = calcNet(xValue, wIn, b);
+
+  const net = calcNet(xValue, weights, bios);
   const out = calcOut(net);
-  //   console.log("wIn:" + wIn);
-  //   console.log("net:" + net);
-  if (xAnswer == out) {
-    printOneRound(xValue, learningCurve, net, out, wIn, b);
+  if (target == out) {
+    // printOneRound(xValue, LEARNING_RATE, net, out, weights, bios);
+    // Not touching to weights and bios
     return {
-      changedWeight: wIn,
-      changedBios: b,
+      changedWeight: weights,
+      changedBios: bios,
+      out,
+      net,
     };
   } else {
-    console.log("have to change weight and bios");
-    xValue.forEach((x, i) => {
-      changedWeight.push(changeWeight(wIn[i], x, xAnswer));
+    // Update weights
+    xValue.forEach((currentX, i) => {
+      changedWeight.push(changeWeight(weights[i], currentX, target));
     });
-    changedBios = changeBios(b, xAnswer);
+    // Update bios
+    changedBios = changeBios(bios, target);
 
-    printOneRound(xValue, learningCurve, net, out, changedWeight, changedBios);
+    // printOneRound(xValue, LEARNING_RATE, net, out, changedWeight, changedBios);
 
+    // Touch and change weights and bios
     return {
       changedWeight,
       changedBios,
+      out,
+      net,
     };
   }
 };
 
-let current = 0;
-while (current !== 1000) {
-  console.log("salam");
-  const mainWeightsBios = [currentWIn, currentBios];
-  let flag = true;
-  for (let xInAnswerQuest of xOrQuest) {
-    const { changedWeight, changedBios } = calcOneRound(
-      currentBios,
-      xInAnswerQuest,
-      currentWIn
-    );
+// Render card to HTML
+const renderCard = (data, number) => {
+  console.log(data);
+  const cardContainer = document.querySelector(".card__container");
+  const cardTemplate = document.getElementById("card-template");
+  const cardClone = cardTemplate.content.cloneNode(true);
+  const card = cardClone.querySelector(".card");
 
-    console.log("--------------------------");
-    currentWIn = changedWeight;
-    currentBios = changedBios;
+  const cardRowTemplate = card.querySelector("#card__row-template");
+  for (let i = 0; i < 4; i++) {
+    const cardRowClone = cardRowTemplate.content.cloneNode(true);
+    const divs = cardRowClone.querySelectorAll(".card__row div");
+    divs[0].textContent = `[${data.xIns[i].join(",")}]`;
+    divs[1].textContent = data.nets[i];
+    divs[2].textContent = data.outs[i];
+    divs[3].textContent = data.targets[i];
+    divs[4].textContent = `[${data.weights[i].join(",")},${data.bioses[i]}]`;
 
-    if (isEqualArrays(mainWeightsBios, [currentWIn, currentBios])) continue;
-    flag = false;
+    card.appendChild(cardRowClone);
   }
-  if (flag) break;
-  current++;
-}
-console.log(current);
+  const cardNumber = card.querySelector(".card__number");
+  cardNumber.textContent = number;
+  cardContainer.appendChild(card);
+};
+
+// Check is weights equal to first one?
+const isEqualArrays = (firstWeight, currentWeight) => {
+  const result = firstWeight.every((_, i) => {
+    return firstWeight[i] === currentWeight[i];
+  });
+  //   console.log(result);
+  //   console.log(firstWeight, currentWeight);
+  return result;
+};
+
+const startProcess = gate => {
+  let counter = 1;
+  let storedData = { ...STORED_DATA_DEFAULT };
+  while (counter !== COUNTER_LIMIT + 1) {
+    const firstWeightsBios = [...currentWeights, currentBios];
+    let isFinish = true;
+    for (let [target, ...xIn] of GATE[gate]) {
+      const { changedWeight, changedBios, out, net } = calcOneRound(
+        currentBios,
+        target,
+        xIn,
+        currentWeights
+      );
+      // Stroe data in array, after finish complete round render card with data.
+      storedData.xIns.push(xIn);
+      storedData.nets.push(net);
+      storedData.outs.push(out);
+      storedData.targets.push(target);
+      storedData.weights.push(changedWeight);
+      storedData.bioses.push(changedBios);
+
+      // Update weight and bios
+      currentWeights = changedWeight;
+      currentBios = changedBios;
+
+      // Check finish condition
+      if (isEqualArrays(firstWeightsBios, [...currentWeights, currentBios]))
+        continue;
+      isFinish = false;
+    }
+    // Render card
+    renderCard(storedData, counter);
+
+    // Reset store data
+    storedData = {
+      xIns: [],
+      nets: [],
+      outs: [],
+      targets: [],
+      weights: [],
+      bioses: [],
+    };
+
+    // Finish condition
+    if (isFinish) break;
+    counter++;
+  }
+};
